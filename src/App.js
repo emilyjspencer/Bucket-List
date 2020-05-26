cf68291b9ba6b5e4274207137b0f0da38f2f27ad
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-05-26 20:25:42 +0100

    Adds event handler and 'dummy' data

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ const App = () =>  {
   ];
 
 
+
   return (
     <div className="bucket-list">
       <h2>Bucket List</h2>
--- src/components/AddItem/AddItem.js
+++ src/components/AddItem/AddItem.js
@@ -2,8 +2,19 @@ import React from 'react';
 import './AddItem.css';
 
 const AddItem = () => {
+
+    const addItemHandler = (event) => {
+        event.preventDefault();
+        const newItem = {
+            id: Math.random().toString(),
+            text: 'Random Item'
+          };
+          console.log(newItem);
+    };
+
+
   return (
-    <form className="new-item">
+    <form className="new-item" onSubmit={addItemHandler}>
       <input type="text" />
       <button type="Submit">Add Item</button>
     </form>
