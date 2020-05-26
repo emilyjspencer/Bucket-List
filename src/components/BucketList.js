c24d5639bb6b6191c7253d3b808420940406178f
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-05-26 19:29:37 +0100

    Adds key prop

--- src/components/BucketList.js
+++ src/components/BucketList.js
@@ -4,13 +4,12 @@ import './BucketList.css';
 
 
 const BucketList = (props) => {
-
     return (
-            <ul className="bucket-list">
-                {props.items.map(item => {
-                    return <li>{item.text}</li>;
-                })} 
-            </ul>
+        <ul className="bucket-list">
+            {props.items.map(item => {
+                return <li key={item.id}>{item.text}</li>;
+            })} 
+        </ul>
     );
 };
 
