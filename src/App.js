d350420f94f2b933c3ae95bd96e354f618985ddd
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-05-26 16:05:27 +0100

    Adds BucketList component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import BucketList from './components/BucketList';
 
 function App() {
   return (
     <div className="bucket-list">
-      <h1>Bucket List</h1>
-      <ul className="items">
-        <li>Karen Blixen Museum, Kenya</li>
-        <li>Bali</li>
-        <li>NYC</li>
-        <li>Northern Lights</li>
-      </ul>
+      <BucketList />
     </div>
   );
 }
---
+++ src/components/BucketList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const BucketList = () => {
+    return (
+        <div>
+            <h1 className="bucket-list">Bucket List</h1>
+            <ul className="items">
+              <li>Karen Blixen Museum, Kenya</li>
+              <li>NYC</li>
+              <li>Bali</li>
+              <li>Northern Lights</li>
+            </ul>
+        </div>
+    )
+}
+
+export default BucketList
