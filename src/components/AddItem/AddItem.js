65fe0497dc22c38e6117d6b77b10d86a0aeb3f7f
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-05-26 23:36:41 +0100

    Adds the ability for a user to enter a bucket list item and see it on the page

--- src/components/AddItem/AddItem.js
+++ src/components/AddItem/AddItem.js
@@ -3,20 +3,28 @@ import './AddItem.css';
 
 const AddItem = (props) => {
 
+    let userInput = '';
+
     const addItemHandler = (event) => {
         event.preventDefault();
         const newItem = {
             id: Math.random().toString(),
-            text: 'Random Item'
+            text: userInput
           };
+
+          console.log(userInput);
+
           console.log(newItem);
           props.onAddItem(newItem);
     };
 
+    const textChangeHandler = event => {
+        userInput = event.target.value;
+    }
 
   return (
     <form className="new-item" onSubmit={addItemHandler}>
-      <input type="text" />
+      <input type="text" onChange={textChangeHandler} />
       <button type="Submit">Add Item</button>
     </form>
   )
